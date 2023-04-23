a904eb7170fbfe248613773a2d390af3ac30ecea
Cat-JiYoung <kimyamae82@gmail.com>
2023-04-23 19:31:17 +0900

    feat(assignment): ComposeQuadrant 과제 구현

--- week1/practice/ComposeQuadrant/app/src/main/java/com/example/composequadrant/MainActivity.kt
+++ week1/practice/ComposeQuadrant/app/src/main/java/com/example/composequadrant/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.composequadrant.ui.theme.ComposeQuadrantTheme
@@ -80,7 +81,8 @@ private fun ComposableInfoCard(
             modifier = Modifier.padding(bottom = 16.dp)
         )
         Text(
-            text = description
+            text = description,
+            textAlign = TextAlign.Justify
         )
     }
 }
